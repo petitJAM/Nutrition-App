eb83819caa2f8ac03ecbce65c562ec95a09f7979
Alex Petitjean <petitjam@rose-hulman.edu>
2012-04-18 23:33:50 -0400

    created empty classes

---
+++ Nutrition-App/src/app/nutrition/NGramModel.java
@@ -0,0 +1,14 @@
+package app.nutrition;
+
+/**
+ * TODO Put here a description of what this class does.
+ *
+ * @author Alex Petitjean.
+ *         Created Apr 13, 2012.
+ */
+public class NGramModel {
+	
+	public NGramModel() {
+		
+	}
+}
---
+++ Nutrition-App/src/app/nutrition/TransitionMatrix.java
@@ -0,0 +1,27 @@
+package app.nutrition;
+
+/**
+ * TODO Put here a description of what this class does.
+ *
+ * @author Alex Petitjean.
+ *         Created Apr 18, 2012.
+ */
+public class TransitionMatrix {
+	
+	/**
+	 * Create a new TransitionMatrix with size N x N filled with 1.
+	 *
+	 * @param N
+	 */
+	public TransitionMatrix(int N) {
+	}
+	
+	/**
+	 * Create a new TransitionMatrix with size N x N filled with fill
+	 *
+	 * @param N
+	 * @param fill
+	 */
+	public TransitionMatrix(int N, int fill) {
+	}
+}
