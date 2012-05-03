af8924fc3262e405dc4ce0ccecf7b19f9816e610
Alex Petitjean <petitjam@rose-hulman.edu>
2012-05-03 12:23:46 -0400

    added the test class for resultsview

---
+++ Nutrition-App-Test/src/app/nutrition/test/ResultsViewActivityTest.java
@@ -0,0 +1,27 @@
+package app.nutrition.test;
+
+import android.test.ActivityInstrumentationTestCase2;
+import app.nutrition.ResultsViewActivity;
+
+/**
+ * Test the ResultsViewActivity class.
+ *
+ * @author Alex Petitjean.
+ *         Created May 3, 2012.
+ */
+public class ResultsViewActivityTest extends ActivityInstrumentationTestCase2<ResultsViewActivity> {
+
+	/**
+	 * Constructor
+	 *
+	 * @param activityClass
+	 */
+	public ResultsViewActivityTest(Class<ResultsViewActivity> activityClass) {
+		super(activityClass);
+	}
+	
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+}
