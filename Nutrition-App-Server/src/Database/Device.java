fd8e2ffbd83bac85c94c7b465db6eb25883fd4ae
Ryne Bell <bellrj@rose-hulman.edu>
2012-05-03 20:08:03 -0400

    added comments to the device and food classes in the database package of the server

--- Nutrition-App-Server/src/Database/Device.java
+++ Nutrition-App-Server/src/Database/Device.java
@@ -1,11 +1,37 @@
 package Database;
 
+/**
+ * A class to represent devices that connect to our system to keep track of
+ * interesting statistics and stuff
+ * 
+ * @author bellrj
+ * 
+ */
 public class Device {
 
+	/**
+	 * the number of times we correctly identified the type of food
+	 */
 	public int times_correct;
+	/**
+	 * the number of times this device has used our service
+	 */
 	public int times_used;
+	/**
+	 * the integer identifier for this device
+	 */
 	public int id;
 
+	/**
+	 * constructor
+	 * 
+	 * @param id
+	 *            the unique identifier for this device
+	 * @param times_used
+	 *            the number of times this device has used our service
+	 * @param times_correct
+	 *            the number of times we correctly identified the type of food
+	 */
 	public Device(int id, int times_used, int times_correct) {
 		this.id = id;
 		this.times_used = times_used;
--- Nutrition-App-Server/src/Database/Food.java
+++ Nutrition-App-Server/src/Database/Food.java
@@ -1,18 +1,84 @@
 package Database;
 
+/**
+ * a class to hold both the transition matrix, and nutrition facts for a given
+ * food item
+ * 
+ * @author bellrj
+ * 
+ */
 public class Food {
 
+	/**
+	 * the matrix describing the image of this food item
+	 */
 	public byte[] Transition_Matrix;
+	/**
+	 * the name of this food item
+	 */
 	public String name;
+	/**
+	 * the total number of calories contained in one serving of this food item
+	 */
 	public float calories;
+	/**
+	 * the number of calories from fat contained in one serving of this food
+	 * item
+	 */
 	public float calFromFat;
+	/**
+	 * the total amount of fat (//TODO in grams?) contained in one serving of
+	 * this food item
+	 */
 	public float totalFat;
+	/**
+	 * the total amount of sodium (in mg) in one serving of this food item
+	 */
 	public float sodium;
+	/**
+	 * the number of carbohydrates in one serving of this food item
+	 */
 	public float carbs;
+	/**
+	 * the amount of fiber (//TODO in grams?) contained in one serving of this
+	 * food item
+	 */
 	public float fiber;
+	/**
+	 * the amount of sugar (in grams) contained in one serving of this food item
+	 */
 	public float sugar;
+	/**
+	 * the amount of protein (//TODO in grams?) contained in one serving of this
+	 * food item
+	 */
 	public float protein;
 
+	/**
+	 * constructor for a food object
+	 * 
+	 * @param bs
+	 *            The transition matrix for this food object
+	 * @param string
+	 *            the name of this food object
+	 * @param calories
+	 *            the number of calories in one serving of this food
+	 * @param calFromFat
+	 *            the number of calories from fat in one serving of this food
+	 * @param totalFat
+	 *            the total amount of fat in one serving of this food item
+	 * @param sodium
+	 *            the total amount of sodium in one serving of this food item
+	 * @param carbs
+	 *            the total amount of carbohydrates in one serving of this food
+	 *            item
+	 * @param fiber
+	 *            the amount of fiber in one serving of this food
+	 * @param sugar
+	 *            the amount of sugar in one serving of this food
+	 * @param protein
+	 *            the amount of protein in one serving of this food
+	 */
 	public Food(byte[] bs, String string, float calories, float calFromFat,
 			float totalFat, float sodium, float carbs, float fiber,
 			float sugar, float protein) {
@@ -28,6 +94,14 @@ public class Food {
 		this.protein = protein;
 	}
 
+	/**
+	 * compares this to another food object, and returns true if all fields are
+	 * the same, false otherwise
+	 * 
+	 * @param f
+	 *            the food to compare this to
+	 * @return true if they are the same, false otherwise
+	 */
 	public boolean equals(Food f) {
 		return compareMatrix(f.Transition_Matrix, this.Transition_Matrix)
 				&& f.name.equals(this.name) && f.calories == this.calories
