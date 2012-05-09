b889dac3781fa2880b41a81e5f7e2d63233fe45e
Rob Wagner <wagnerrj@rose-hulman.edu>
2012-05-08 23:10:34 -0400

    Completely redid the gui

--- Nutrition-App/gen/app/nutrition/R.java
+++ Nutrition-App/gen/app/nutrition/R.java
@@ -12,21 +12,33 @@ public final class R {
     }
     public static final class drawable {
         public static final int app_logo=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int background=0x7f020001;
+        public static final int cammerabutton=0x7f020002;
+        public static final int exitbutton=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
+        public static final int infobutton=0x7f020005;
+        public static final int searchbutton=0x7f020006;
+        public static final int settingsbutton=0x7f020007;
     }
     public static final class id {
-        public static final int about_us=0x7f050000;
         public static final int camera_button=0x7f050001;
-        public static final int logo_view=0x7f050002;
+        public static final int exit_button=0x7f050005;
+        public static final int info_button=0x7f050004;
+        public static final int logo_view=0x7f050000;
+        public static final int search_button=0x7f050002;
+        public static final int settings_button=0x7f050003;
     }
     public static final class layout {
         public static final int main=0x7f030000;
         public static final int results=0x7f030001;
     }
     public static final class string {
-        public static final int about_us_text=0x7f040003;
         public static final int app_name=0x7f040000;
         public static final int camera_button_text=0x7f040002;
+        public static final int exit_button_text=0x7f040006;
+        public static final int info_button_text=0x7f040005;
         public static final int logo_content_description=0x7f040001;
+        public static final int search_button_text=0x7f040003;
+        public static final int settings_button_text=0x7f040004;
     }
 }
