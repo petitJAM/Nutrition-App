ddbaf78a547d81c743f260a36cc4e4b317379470
Rob Wagner <wagnerrj@rose-hulman.edu>
2012-05-10 22:51:49 -0400

    Added some more tests to make sure NutritionAppActivity initializes everything appropriately

--- Nutrition-App-Test/src/app/nutrition/test/NutritionAppTest.java
+++ Nutrition-App-Test/src/app/nutrition/test/NutritionAppTest.java
@@ -3,6 +3,7 @@ package app.nutrition.test;
 import android.test.ActivityInstrumentationTestCase2;
 import android.widget.ImageButton;
 import app.nutrition.NutritionAppActivity;
+import app.nutrition.R;
 
 /**
  * Test the NutritionAppActivity class
@@ -15,7 +16,7 @@ public class NutritionAppTest extends
 
 	private NutritionAppActivity naa;
 	private String appName;
-	private ImageButton camera_button;
+	private ImageButton camera, set, info, search, exit;
 	
 	/**
 	 * Constructor.
@@ -32,7 +33,11 @@ public class NutritionAppTest extends
 
 		setActivityInitialTouchMode(false);
 		naa = getActivity();
-		camera_button = (ImageButton) naa.findViewById(app.nutrition.R.id.camera_button);
+		camera = (ImageButton) naa.findViewById(app.nutrition.R.id.camera_button);
+		set = (ImageButton) naa.findViewById(R.id.settings_button);
+		info = (ImageButton) naa.findViewById(R.id.info_button);
+		search = (ImageButton) naa.findViewById(R.id.search_button);
+		exit = (ImageButton) naa.findViewById(R.id.exit_button);
 		appName = naa.getString(app.nutrition.R.string.app_name);
 	}
 
@@ -49,5 +54,42 @@ public class NutritionAppTest extends
 	public void testAppName() {
 		assertEquals("Nutrition App", appName);
 	}
+	
+	/**
+	 * Test that camera button created
+	 */
+	public void testCameraCreation() {
+		assertNotNull(camera);
+	}
+	
+	/**
+	 * Test that settings button created
+	 */
+	public void testSettingsCreation() {
+		assertNotNull(set);
+	}
+	
+	/**
+	 * Test that info button created
+	 */
+	public void testInfoCreation() {
+		assertNotNull(info);
+	}
+	
+	/**
+	 * Test that search button created
+	 */
+	public void testSearchCreation() {
+		assertNotNull(search);
+	}
+	
+	/**
+	 * Test that exit button created
+	 */
+	public void testExitCreation() {
+		assertNotNull(exit);
+	}
+	
+
 	
 }
