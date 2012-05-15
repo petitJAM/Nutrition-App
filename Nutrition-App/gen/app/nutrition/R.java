7bdcd742776b480623a03b60915394c88276c031
Alex Petitjean <petitjam@rose-hulman.edu>
2012-05-14 21:04:52 -0400

    added empty NutritionDisplayActivity class for the obvious reason

--- Nutrition-App/gen/app/nutrition/R.java
+++ Nutrition-App/gen/app/nutrition/R.java
@@ -33,8 +33,9 @@ public final class R {
     public static final class layout {
         public static final int info=0x7f030000;
         public static final int main=0x7f030001;
-        public static final int results=0x7f030002;
-        public static final int results_list=0x7f030003;
+        public static final int nutrition_display=0x7f030002;
+        public static final int results=0x7f030003;
+        public static final int results_list=0x7f030004;
     }
     public static final class string {
         public static final int about_us=0x7f05000b;
